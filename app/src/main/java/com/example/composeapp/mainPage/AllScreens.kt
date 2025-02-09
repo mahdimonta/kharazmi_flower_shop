1005c8cbef9d44693d3ccfb6c7a093a9aedf4d76
git config --global user.name mahdimonta67git config --global user.name <mahdimont67@gmail.com>
2025-02-09 13:07:50 +0330

    some changes

--- app/src/main/java/com/example/composeapp/mainPage/AllScreens.kt
+++ app/src/main/java/com/example/composeapp/mainPage/AllScreens.kt
@@ -1,23 +1,19 @@
 
 package com.example.composeapp.mainPage
 
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
